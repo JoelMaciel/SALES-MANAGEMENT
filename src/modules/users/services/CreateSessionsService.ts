4489d6aa5aea8123498d34a41b59ad1100760428
JoelMaciel <soldado25461@gmail.com>
2024-05-17 21:49:36 -0500

    feat:  implementing authentication with JWT

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,14 +3,21 @@ import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import AppError from "@shared/errors/AppError";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import authConfig from "@config/auth";
 
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const userRepository = getCustomRepository(UsersRepository);
     const user = await userRepository.findByEmail(email);
 
@@ -24,7 +31,15 @@ class CreateSessionsService {
       throw new AppError("Incorret email or password combination", 401);
     }
 
-    return user;
+    const token = sign({}, authConfig.jwt.secret, {
+      subject: user.id,
+      expiresIn: authConfig.jwt.expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
