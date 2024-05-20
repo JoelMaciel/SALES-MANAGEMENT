27e58bc30b31fba150a453dd5186e158fca50619
JoelMaciel <soldado25461@gmail.com>
2024-05-19 20:56:28 -0500

    feat:  template handlebars for email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from "typeorm";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
 import AppError from "@shared/errors/AppError";
+import EtherealMail from "@config/mail/EtherealMail";
 
 interface IRequest {
   email: string;
@@ -19,7 +20,23 @@ class SendForgotPasswordEmailService {
     }
 
     const token = await userTokensRepository.generate(user.id);
+
     console.log(token);
+
+    await EtherealMail.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: "[SALES API] Password Recovery",
+      templateData: {
+        template: `Hello {{name}}: {{token}}`,
+        variables: {
+          name: user.name,
+          token: token?.token,
+        },
+      },
+    });
   }
 }
 
